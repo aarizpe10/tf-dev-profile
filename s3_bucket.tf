d2bf56863a3c45da5f383e5602708bfe5a51fc62
Adrian <adrian.arizpe10@gmail.com>
2024-03-11 13:53:40 -0500

    test

--- s3_bucket.tf
+++ s3_bucket.tf
@@ -1,4 +1,4 @@
 resource "aws_s3_bucket" "my_bucket" {
   bucket = var.AWS_BUCKETNAME
-  acl    = "private"  # Access control list for the bucket (private by default)
+  acl    = "private"  # Access control list for the bucket 
 }
