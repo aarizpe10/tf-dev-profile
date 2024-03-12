b5944943133208e67ed380f0c90de8330af15188
Adrian <adrian.arizpe10@gmail.com>
2024-03-11 22:01:35 -0500

    test

---
+++ route53.tf
@@ -0,0 +1,49 @@
+module "zones" {
+  source  = "terraform-aws-modules/route53/aws//modules/zones"
+  version = "~> 2.0"
+
+  zones = {
+    "terraform-aws-modules-example.com" = {
+      comment = "terraform-aws-modules-examples.com (production)"
+      tags = {
+        env = "production"
+      }
+    }
+
+    "myapp.com" = {
+      comment = "myapp.com"
+    }
+  }
+
+  tags = {
+    ManagedBy = "Terraform"
+  }
+}
+
+module "records" {
+  source  = "terraform-aws-modules/route53/aws//modules/records"
+  version = "~> 2.0"
+
+  zone_name = keys(module.zones.route53_zone_zone_id)[0]
+
+  records = [
+    {
+      name    = "hostedSZone"
+      type    = "A"
+      alias   = {
+        name    = "d-10qxlbvagl.execute-api.${vars.AWS_REGION}.amazonaws.com"
+        zone_id = "Z015395639Y993ORQ7P9"
+      }
+    },
+    {
+      name    = ""
+      type    = "A"
+      ttl     = 3600
+      records = [
+        "10.10.10.10",
+      ]
+    },
+  ]
+
+  depends_on = [module.zones]
+}
