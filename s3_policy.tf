ca7631430bd97b8a0f31399435ad5aa706ecf92f
Adrian <adrian.arizpe10@gmail.com>
2024-03-11 21:30:18 -0500

    test

--- s3_policy.tf
+++ s3_policy.tf
@@ -1,22 +1,15 @@
 # S3 bucket policy
 resource "aws_s3_bucket_policy" "bucket-policy" {
   bucket = aws_s3_bucket.angular-bucket.id
-  policy = data.aws_iam_policy_document.allow_access_from_another_account.json
-}
-data "aws_iam_policy_document" "allow_access_from_another_account" {
-  statement {
-    principals {
-      type        = "AWS"
-      identifiers = ["123456789012"]
-    }
-
-    actions = [
-      "s3:*",
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:*"
+        Resource  = "${aws_s3_bucket.angular-bucket.arn}/*"
+      }
     ]
-
-    resources = [
-      aws_s3_bucket.angular-bucket.arn,
-      "${aws_s3_bucket.angular-bucket.arn}/*",
-    ]
-  }
+  })
 }
