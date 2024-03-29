296a66f21d0641b4720a6f1aa5b7a8dc5462903d
Adrian <adrian.arizpe10@gmail.com>
2024-03-29 10:40:39 -0500

    added output for zone id

--- route53.tf
+++ route53.tf
@@ -19,7 +19,7 @@ module "zones" {
 module "records" {
   source  = "terraform-aws-modules/route53/aws//modules/records"
   version = "~> 2.0"
-  zone_name = keys(module.zones.route53_zone_zone_id)[0]
+  zone_name = module.zones.route53_zone_zone_id
 
   records = [
     {
