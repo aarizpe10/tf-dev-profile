d950d4f348d9271f19a377273114bdd465f65661
Adrian <adrian.arizpe10@gmail.com>
2024-04-21 18:32:37 -0500

    test

--- s3_bucket.tf
+++ s3_bucket.tf
@@ -21,7 +21,6 @@ resource "aws_s3_bucket_website_configuration" "angular-bucket" {
     key = "error.html"
   }
 }
-
 # S3 bucket ACL access
 
 resource "aws_s3_bucket_ownership_controls" "angular-bucket" {
@@ -33,7 +32,6 @@ resource "aws_s3_bucket_ownership_controls" "angular-bucket" {
 
 resource "aws_s3_bucket_public_access_block" "angular-bucket" {
   bucket = aws_s3_bucket.angular-bucket.id
-
   block_public_acls       = false
   block_public_policy     = false
   ignore_public_acls      = false
@@ -48,4 +46,27 @@ resource "aws_s3_bucket_acl" "angular-bucket" {
 
   bucket = aws_s3_bucket.angular-bucket.id
   acl    = "public-read"
+}
+
+resource "aws_s3_bucket_policy" "allow_access_from_another_account" {
+  bucket = aws_s3_bucket.angular-bucket.id
+  policy = data.aws_iam_policy_document.allow_access_from_another_account.json
+}
+
+data "aws_iam_policy_document" "allow_access_from_another_account" {
+  statement {
+    principals {
+      type        = "AWS"
+      identifiers = ["123456789012"]
+    }
+
+    actions = [
+      "s3:*"
+    ]
+
+    resources = [
+      aws_s3_bucket.angular-bucket.arn,
+      "${aws_s3_bucket.angular-bucket.arn}/*",
+    ]
+  }
 }
