b752384421cfcfa0ed1b85f7b5f73c19164a4c0d
Adrian <adrian.arizpe10@gmail.com>
2024-04-26 11:39:40 -0500

    test

--- route53.tf
+++ route53.tf
@@ -16,11 +16,15 @@ module "zones" {
   }
 }
 
+data "aws_route53_zone" "angular" {
+  name = "adrian-arizpe-test.com"
+}
+
 module "records" {
   source    = "terraform-aws-modules/route53/aws//modules/records"
   version   = "~> 2.0"
   zone_name = keys(module.zones.route53_zone_zone_id)[0]
-  zone_id = data.aws_route53_zone.angular["Z015395639Y993ORQ7P96"]
+  zone_id   = data.aws_route53_zone.angular.zone_id
 
   records = [
     {
