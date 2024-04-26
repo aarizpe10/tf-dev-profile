ab7826303514f9ae12e08c0b57ccc0a2942914f0
Adrian <adrian.arizpe10@gmail.com>
2024-04-26 11:32:38 -0500

    test

--- main.tf
+++ main.tf
@@ -12,7 +12,11 @@ provider "aws" {
   access_key = var.AWS_ACCESS_KEY_ID
   secret_key = var.AWS_SECRET_ACCESS_KEY
 }
+data "aws_route53_zones" "all_zones" {}
 
 data "aws_route53_zone" "angular" {
-  vpc_id = "Z015395639Y993ORQ7P96"
-}
+  for_each = toset([
+    "Z015395639Y993ORQ7P96",
+  ])
+  zone_id = each.key
+  }
