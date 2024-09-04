2e407964a02277d3422bfa1b5ad5f1a284e6c90f
Adrian Arizpe <adriansmac@Adrians-MacBook-Pro.local>
2024-09-04 18:05:20 -0500

    test

--- route53.tf
+++ route53.tf
@@ -8,6 +8,9 @@ resource "aws_route53_record" "angular" {
   ttl     = 300
   records = [
     aws_route53_zone.angular.name_servers[0],
-    aws_route53_zone.angular.name_servers[1]
+    aws_route53_zone.angular.name_servers[1].
+    aws_route53_zone.angular.name_servers[3],
+    aws_route53_zone.angular.name_servers[4]
+
   ]
 }
