951a5f6cacba97912c02be4655991cfdac4cbe73
Adrian Arizpe <adriansmac@Adrians-MacBook-Pro.local>
2024-09-04 18:41:42 -0500

    test

--- cloudfront.tf
+++ cloudfront.tf
@@ -9,7 +9,7 @@ locals {
 
 resource "aws_cloudfront_distribution" "s3_distribution" {
   origin {
-    domain_name              = "testings3arizpe.s3-website.us-east-2.amazonaws.com"
+    domain_name              = aws_acm_certificate.cert.domain_name
     # origin_access_control_id = aws_cloudfront_origin_access_control.default.id
     origin_id                = local.s3_origin_id
   }
