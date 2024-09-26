e6be9621b8d1cd82565cbd28161a429ef1922e0b
Adrian Arizpe <adriansmac@dhcp-10-184-128-149.utsarr.net>
2024-09-26 11:09:58 -0500

    test

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket         = "terraformdevopsstatefile"
+    bucket         = var.AWS_BACKEND_BUCKET
     key            = "backend/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform_statefile_lock"
